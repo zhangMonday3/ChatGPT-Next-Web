519c8bcd7244a3c454bc1dcdb4ed3d4fe2067b5f
zhangMonday3 <146724504+zhangMonday3@users.noreply.github.com>
2023-12-30 12:11:21 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          ChatGPT X zzybot
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          由zzy发电的ChatGPT公益镜像
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -217,7 +217,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+            <a href={"https://www.zhangmonday.top"} target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
